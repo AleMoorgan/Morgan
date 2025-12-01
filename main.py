ac33a562f57c5eea10a99bd25ae145e85aaa5336
AleMoorgan <aangelresendizhdz@gmail.com>
2025-11-30 20:54:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def suma(a: int, b: int) -> int:
-    return a + b
+"""Este módulo imprime un mensaje simple para probar pylint."""
 
+def main():
+    """Función principal que imprime un mensaje."""
+    print("Hola mundo, probando pylint")
+    
 if __name__ == "__main__":
-    print("Resultado:", suma(3, 5))
+    main()
